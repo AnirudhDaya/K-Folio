912f5fece81b60ba234eb74420170b05f5f16dfb
Aniz <20am014@gmail.com>
2023-04-06 13:10:32 +0530

    made resoponsive

--- src/Components/ComingSoon/ComingSoon.jsx
+++ src/Components/ComingSoon/ComingSoon.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./ComingSoon.css";
+import { Link } from "react-router-dom";
 
 function ComingSoon() {
   return (
-    <div>
+    <div className="comingSoonContainer">
       <div className="BigLogo mainLogo">Coming Soon</div>
-      <p>This page shows all the projects related to that particular stack</p>
+
+      <p className="comingSoonPara">
+        This page shows all the projects related to that particular stack
+      </p>
     </div>
   );
 }
