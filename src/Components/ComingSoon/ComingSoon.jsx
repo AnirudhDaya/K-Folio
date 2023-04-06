10ea337606756fb78743b078f2d377be2250a4e4
Aniz <20am014@gmail.com>
2023-04-06 13:04:55 +0530

    coming soon added

--- src/Components/ComingSoon/ComingSoon.jsx
+++ src/Components/ComingSoon/ComingSoon.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import './ComingSoon.css'
+import React from "react";
+import "./ComingSoon.css";
 
 function ComingSoon() {
   return (
-    <div className = "BigLogo mainLogo">Coming Soon</div>
-  )
+    <div>
+      <div className="BigLogo mainLogo">Coming Soon</div>
+      <p>This page shows all the projects related to that particular stack</p>
+    </div>
+  );
 }
 
-export default ComingSoon
+export default ComingSoon;
